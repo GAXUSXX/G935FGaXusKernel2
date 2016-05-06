00ac4cfe2d5ff4193897280b2e55f36b4bd3f28f
GAXUSXX <mchnet3@gmail.com>
2016-05-06 16:04:49 +0900

    defconfig: update defconfig

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Fri May 6 15:49:53 JST 2016"
+#define UTS_VERSION "#3 SMP PREEMPT Fri May 6 15:54:52 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.18.14xx-1.0-gcc44228-dirty"
+#define UTS_RELEASE "3.18.14-GaXusKernel-hero2lte-xx-1.0"
