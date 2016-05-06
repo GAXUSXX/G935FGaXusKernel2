626c5559e4fa7d60c144314647b1bd19fb837c20
GAXUSXX <mchnet3@gmail.com>
2016-05-06 16:59:34 +0900

    Fix Build Error Decon etc..

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#3 SMP PREEMPT Fri May 6 15:54:52 JST 2016"
+#define UTS_VERSION "#6 SMP PREEMPT Fri May 6 16:58:49 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
