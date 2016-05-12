5d451573579a91a6057809aa298e632080473c2e
GAXUSXX <mchnet3@gmail.com>
2016-05-12 12:57:01 +0900

    cpufreq: Add scaling_available_frequencies

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 22 */
+/* This file is auto generated, version 23 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#22 SMP PREEMPT Fri May 6 20:17:52 JST 2016"
+#define UTS_VERSION "#23 SMP PREEMPT Thu May 12 12:51:49 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
