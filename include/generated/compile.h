abf6c1a92465f86645179133eb169a73d3161993
GAXUSXX <mchnet3@gmail.com>
2016-05-13 23:29:39 +0900

    Add Sweep2sleep etc...

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 28 */
+/* This file is auto generated, version 29 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#28 SMP PREEMPT Fri May 13 22:42:56 JST 2016"
+#define UTS_VERSION "#29 SMP PREEMPT Fri May 13 22:56:05 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
